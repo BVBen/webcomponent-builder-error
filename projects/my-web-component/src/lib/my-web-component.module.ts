587f7224a509dbe03e02ed441a832483e0d6ddce
Benjamin Blömer <benjamin.bloemer@datev.de>
2024-04-23 10:44:16 +0200

    fix packages json

--- projects/my-web-component/src/lib/my-web-component.module.ts
+++ projects/my-web-component/src/lib/my-web-component.module.ts
@@ -1,16 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { MyWebComponentComponent } from './my-web-component.component';
-
-
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
     MyWebComponentComponent
   ],
   imports: [
+    BrowserModule
   ],
-  exports: [
+  entryComponents: [
     MyWebComponentComponent
-  ]
+  ],
+  bootstrap: []
 })
-export class MyWebComponentModule { }
+export class MyWebComponentModule { 
+  constructor(private injector: Injector) { }
+
+  public ngDoBootstrap() {
+    
+    let component;
+
+    component = createCustomElement(MyWebComponentComponent, { injector: this.injector });
+    customElements.define('my-web-component', component);
+  }
+}
--- projects/my-web-component/src/public-api.ts
+++ projects/my-web-component/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of my-web-component
  */
 
-export * from './lib/my-web-component.service';
 export * from './lib/my-web-component.component';
 export * from './lib/my-web-component.module';
